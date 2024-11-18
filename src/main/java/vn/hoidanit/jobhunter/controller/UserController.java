3794be72a681e1ec0f9bb9b51b2fe1ab0b77cf29
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-18 22:20:11 +0700

    51

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,12 +22,17 @@ public class UserController {
 
     public final UserService userService;
 
-    public UserController(UserService userService) {
+    public final PasswordEncoder passwordEncoder;
+
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
     public ResponseEntity<User> createNewUser(@RequestBody User request) {
+        String encodedPassword = this.passwordEncoder.encode(request.getPassword());
+        request.setPassword(encodedPassword);
         User userRes = userService.handleCreateUser(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userRes);
